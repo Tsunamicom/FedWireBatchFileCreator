71aacd5b44ad95bee87b044845fee75f414fd0d0
Kurtis Mackey <kurtis.g.mackey@gmail.com>
2017-01-13 18:28:19 -0500

    New Method in MandatoryFedWireField class: assignFieldValue() -Based on the passed Tag, Assign the FieldValue.

--- FedWire Batch File Creator/MandatoryFedWireField.cs
+++ FedWire Batch File Creator/MandatoryFedWireField.cs
@@ -16,6 +16,58 @@ namespace FedWire_Batch_File_Creator
         public string RecieverDI { get; set; }
         public string BusinessFunctionCode { get; set; }
 
-        public MandatoryFedWireField(string tag, int maxLength=9999) : base(tag, maxLength){}
+        public MandatoryFedWireField(string tag, int maxLength = 9999) : base(tag, maxLength)
+        {
+            assignFieldValue();
+        }
+        
+
+        private void assignFieldValue()
+        {
+            switch (base.Tag)
+            {
+                case ("1500"):
+                    {
+                        base.FieldValue = SenderSuppliedInformation+"";
+                        break;
+                    }
+                case ("1510"):
+                    {
+                        base.FieldValue = TypeSubType+"";
+                        break;
+                    }
+                case ("1520"):
+                    {
+                        base.FieldValue = IMAD+"";
+                        break;
+                    }
+                case ("2000"):
+                    {
+                        base.FieldValue = Amount+"";
+                        break;
+                    }
+                case ("3100"):
+                    {
+                        base.FieldValue = SenderDI+"";
+                        break;
+                    }
+                case ("3400"):
+                    {
+                        base.FieldValue = RecieverDI+"";
+                        break;
+                    }
+                case ("3600"):
+                    {
+                        base.FieldValue = BusinessFunctionCode+"";
+                        break;
+                    }
+                default:
+                    {
+                        base.FieldValue = "INVALID";
+                        break;
+                    }
+                    
+            }
+        }
     }
 }
