2c3037b401c1bfc4e6a85cb1519b8ca3f9f2f79b
Kurtis Mackey <kurtis.g.mackey@gmail.com>
2017-01-14 15:32:10 -0500

    FedWireField changes -Removed isAllowed and isActive properties -Added new Interface:  IFedWireField

--- FedWire Batch File Creator/FedWireField.cs
+++ FedWire Batch File Creator/FedWireField.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace FedWire_Batch_File_Creator
 {
-    public class FedWireField
+    public class FedWireField : IFedWireField
     {
         private string _FieldValue;
         private string _tag;
-        private bool _isAllowed = true;
-        private bool _isActive = false;
         private int _maxLength = 9999;
         
-        public bool isAllowed { get { return _isAllowed; } set { isAllowed = value; } }
-        public bool isActive { get { return _isActive; } set { _isActive = value; } }
         public string FieldValue { get { return _FieldValue; } set { if (value.Length <= _maxLength) { _FieldValue = value; } } }
         public string Tag { get { return _tag; } }
 
---
+++ FedWire Batch File Creator/IFedWireField.cs
@@ -0,0 +1,8 @@
+﻿namespace FedWire_Batch_File_Creator
+{
+    public interface IFedWireField
+    {
+        string FieldValue { get; set; }
+        string Tag { get; }
+    }
+}
